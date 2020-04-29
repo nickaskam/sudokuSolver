ea5d1d414fd4c4dcd199ee02b345a9715d65ebbb
Nick Askam <asknickfilms@gmail.com>
2020-04-29 11:20:00 -0500

    creating algorithm to check if possible numbers can be the solution

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -11,3 +11,22 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [0,0,0,0,8,0,0,7,9]]
 
 print(np.matrix(grid))
+
+def possible(y,x,n):
+    global grid 
+    for i in range(0,9):
+        if grid[y][i] == n:
+            return False
+    for i in range(0,9):
+        if grid[i][x] == n:
+            return False
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if grid[y0+i][x0+j] == n:
+                return False
+    return True
+
+print("Is the middle square 3? " + str(possible(4,4,3)))
+print("Is the middle square 5? " + str(possible(4,4,5)))
